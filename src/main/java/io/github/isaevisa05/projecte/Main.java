62671a79cc799fcd5ab3722f5b9efc6ec7ce51bf
isaevisa05 <isaevisao55@gmail.com>
2025-10-08 13:43:37 +0300

    Update

--- src/main/java/io/github/isaevisa05/projecte/Main.java
+++ src/main/java/io/github/isaevisa05/projecte/Main.java
@@ -3,6 +3,12 @@ package io.github.isaevisa05.projecte;
 import io.github.isaevisa05.projecte.comparators.ComparatorManager;
 import io.github.isaevisa05.projecte.comparators.enums.StudentComparatorType;
 import io.github.isaevisa05.projecte.comparators.enums.UniversityComparatorType;
+import io.github.isaevisa05.projecte.entity.Student;
+import io.github.isaevisa05.projecte.entity.University;
+import io.github.isaevisa05.projecte.utils.JsonUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -17,6 +23,45 @@ public class Main {
         students.stream()
                 .sorted(ComparatorManager.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE))
                 .forEach(System.out::println);
+
+        String universitiesString = JsonUtil.universitiesToJson(universities);
+        String studentsString = JsonUtil.studentsToJson(students);
+
+
+        System.out.println(universitiesString);
+        System.out.println(studentsString);
+
+        var universities2 = JsonUtil.jsonToUniversities(universitiesString);
+        var students2 = JsonUtil.jsonToStudents(studentsString);
+
+        System.out.println(universities.size() + " -> " + universities2.size());
+        System.out.println(students.size() + " -> " + students2.size());
+
+        List<String> serializedUniversities = new ArrayList<>();
+        for (University university : universities) {
+            serializedUniversities.add(JsonUtil.universityToJson(university));
+        }
+
+        List<String> serializedStudents = new ArrayList<>();
+        for (Student student : students) {
+            serializedStudents.add(JsonUtil.studentToJson(student));
+        }
+
+        universities.stream()
+                .forEach((university -> {
+                    String json = JsonUtil.universityToJson(university);
+                    System.out.println(json);
+                    University university2 = JsonUtil.jsonToUniversity(json);
+                    System.out.println(university2);
+                }));
+
+        students.stream()
+                .forEach((student -> {
+                    String json = JsonUtil.studentToJson(student);
+                    System.out.println(json);
+                    Student student2 = JsonUtil.jsonToStudent(json);
+                    System.out.println(student2);
+                }));
     }
 
 }
--- src/main/java/io/github/isaevisa05/projecte/utils/JsonUtil.java
+++ src/main/java/io/github/isaevisa05/projecte/utils/JsonUtil.java
@@ -2,10 +2,12 @@ package io.github.isaevisa05.projecte.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import io.github.isaevisa05.projecte.entity.Student;
 import io.github.isaevisa05.projecte.entity.University;
 
 import java.util.Collection;
+import java.util.List;
 
 public class JsonUtil {
 
@@ -21,15 +23,26 @@ public class JsonUtil {
         return gson.toJson(university);
     }
 
-    public static String studentsToJson(Collection<Student> students) {
+    public static String studentsToJson(List<Student> students) {
         return gson.toJson(students);
     }
 
-    public static String universitiesToJson(Collection<University> universities) {
+    public static String universitiesToJson(List<University> universities) {
         return gson.toJson(universities);
     }
 
     public static Student jsonToStudent(String json) {
+        return gson.fromJson(json, Student.class);
+    }
 
+    public static University jsonToUniversity(String json) {
+        return gson.fromJson(json, University.class);
+    }
+
+    public static List<Student> jsonToStudents(String json) {
+        return gson.fromJson(json, new TypeToken<List<Student>> () {}.getType());
+    }
+    public static List<University> jsonToUniversities(String json) {
+        return gson.fromJson(json, new TypeToken<List<University>> () {}.getType());
     }
 }
